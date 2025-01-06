61461ce6560e0287afff8c005deeebd79519d427
amit47-eng <av495267@gmail.com>
2025-01-06 10:36:54 +0530

    adding interface

---
+++ src/content/content.jsx
@@ -0,0 +1,18 @@
+import "./content.css";
+
+function Content() {
+
+    return (
+<div className="main-content">
+    <header className="hero-section">
+      <h1>Close every deal.</h1>
+      <p>Radiant helps you sell more by revealing sensitive information about your customers.</p>
+      <div className="buttons">
+        <a href="#" className="button button-black">Get Started</a>
+        <a href="#" className="button button-outline">See Pricing</a>
+      </div>
+    </header>
+</div>
+    )
+};
+export default Content;
